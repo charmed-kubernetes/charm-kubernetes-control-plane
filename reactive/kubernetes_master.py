9594dd12ed783d32173c79b1b4426b23d73d6844
Joe Borg <joseph.borg@canonical.com>
2019-10-11 13:25:21 -0400

    Send pause image path to containerd charm (#64)
    * Send pause image path to containerd charm
    
    * PEP8 fixed
    
    * s/pause_image/sandbox_image
    
    * document where the registry is being sent

--- reactive/kubernetes_master.py
+++ reactive/kubernetes_master.py
@@ -2477,7 +2477,18 @@ def get_dns_provider():
 def send_registry_location():
     registry_location = hookenv.config('image-registry')
     kube_control = endpoint_from_flag('kube-control.connected')
+
+    # Send registry to workers
     kube_control.set_registry_location(registry_location)
+
+    # Construct and send the sandbox image (pause container) to our runtime
+    runtime = endpoint_from_flag('endpoint.container-runtime.available')
+    if runtime:
+        uri = '{}/pause-{}:3.1'.format(registry_location, arch())
+        runtime.set_config(
+            sandbox_image=uri
+        )
+
     set_flag('kubernetes-master.sent-registry')
 
 
