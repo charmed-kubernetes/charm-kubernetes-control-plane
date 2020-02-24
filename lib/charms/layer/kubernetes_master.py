a4d133de92b938840cb6c9c271222ee20fb196cf
Cory Johns <johnsca@gmail.com>
2020-02-24 13:46:44 -0500

    [backport: stable] Add support for CephFS (#76)
    * Add support for CephFS (#74)
    
    * Add support for CephFS
    
    Adds detecting of CephFS when Ceph is related and, if present, informs
    cdk-addons to enable the cephfs-csi plugin.
    
    Part of https://bugs.launchpad.net/charm-kubernetes-master/+bug/1862217
    
    * Use apt_install helper from layer-basic
    
    The helper has optimistic-with-fallback handling of stale package
    indexes and dpkg lock contention.
    
    * Allow for errors from ceph CLI (#75)
    
    If k8s-master is deployed on pre-19.10 without a cloud:train added to
    the install_sources config, the check for CephFS will fail and cause
    errors even if CephFS is not being used.  This resolves that by catching
    the error and defaulting to not detecting CephFS.

--- lib/charms/layer/kubernetes_master.py
+++ lib/charms/layer/kubernetes_master.py
@@ -1,6 +1,12 @@
-from charmhelpers.core import hookenv
-from charms.reactive import endpoint_from_flag, is_flag_set
+import json
+from pathlib import Path
+from subprocess import check_output, CalledProcessError
+from tempfile import TemporaryDirectory
 
+from charmhelpers.core import hookenv
+from charms.reactive import endpoint_from_flag, is_flag_set, set_flag
+
+from charms.layer import basic
 from charms.layer import kubernetes_common
 
 
@@ -64,3 +70,30 @@ def get_api_endpoint(relation=None):
         return (ingress_address, STANDARD_API_PORT)
     else:
         return (hookenv.unit_public_ip(), STANDARD_API_PORT)
+
+
+def query_cephfs_enabled(ceph_ep):
+    if not is_flag_set('kubernetes-master.ceph-cli.installed'):
+        basic.apt_install(['ceph-common'])
+        set_flag('kubernetes-master.ceph-cli.installed')
+    ceph_config = {
+        'hosts': ceph_ep.mon_hosts(),
+        'key': ceph_ep.key(),
+        'auth': ceph_ep.auth(),
+    }
+    with TemporaryDirectory() as tmpdir:
+        conf_file = Path(tmpdir) / 'ceph.conf'
+        conf_file.write_text(
+            '[global]\n'
+            'mon_host = {hosts}\n'
+            'key = {key}\n'
+            'auth cluster required = {auth}\n'
+            'auth service required = {auth}\n'
+            'auth client required = {auth}\n'.format(**ceph_config))
+        try:
+            out = check_output(['ceph', 'mds', 'versions',
+                                '-c', str(conf_file)])
+            return bool(json.loads(out))
+        except CalledProcessError:
+            hookenv.log('Unable to determine if CephFS is enabled', 'ERROR')
+            return False
