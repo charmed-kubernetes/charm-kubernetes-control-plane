031601c5c52a451757c39f262a45a3be7a1ca5ad
hemanthnakkina <mail2hemanth.n@gmail.com>
2020-05-12 21:26:01 +0530

    support for service-cidr expansion (#93)
    kubernetes-master charm does not allow service-cidr to be changed
    once k8s is deployed. However k8s by itself supports service-cidr
    expansion. This PR adds support for service-cidr expansion in
    kubernetes-master charm.
    
    The charm checks whether the new service-cidr is an expansion or not.
    If it is not an expansion, a warning message is displayed on the
    status line.
    
    For expansion case,
    * regenerate API certs
    * update kube-apiserver parameters
    * restart kube-apiserver apiservice
    * delete kubernetes service object
    * rollout cdk-addon services
    
    Closes-Bug: #1868685
    
    Co-authored-by: Hemanth Nakkina <hemanth.nakkina@canonical.com>

--- tests/test_kubernetes_master.py
+++ tests/test_kubernetes_master.py
@@ -1,8 +1,9 @@
 import pytest
 from unittest import mock
 from reactive import kubernetes_master
+from charms.layer.kubernetes_common import get_version, kubectl
 from charms.reactive import endpoint_from_flag, remove_state
-from charmhelpers.core import hookenv
+from charmhelpers.core import hookenv, unitdata
 
 
 def patch_fixture(patch_target):
@@ -36,3 +37,32 @@ def test_series_upgrade():
     kubernetes_master.post_series_upgrade()
     assert kubernetes_master.service_pause.call_count == 4
     assert kubernetes_master.service_resume.call_count == 4
+
+
+@mock.patch('builtins.open', mock.mock_open())
+@mock.patch('os.makedirs', mock.Mock(return_value=0))
+def configure_apiserver(service_cidr_from_db, service_cidr_from_config,
+                        kubectl_call_count):
+    db = unitdata.kv()
+    db.get.return_value = service_cidr_from_db
+    hookenv.config.return_value = service_cidr_from_config
+    get_version.return_value = (1, 18)
+    kubectl.return_value = '{"items": []}'.encode('UTF-8')
+    kubernetes_master.configure_apiserver()
+    assert kubectl.call_count == kubectl_call_count
+
+
+def test_service_cidr_greenfield_deploy():
+    configure_apiserver(None, '10.152.183.0/24', 0)
+
+
+def test_service_cidr_no_change():
+    configure_apiserver('10.152.183.0/24', '10.152.183.0/24', 0)
+
+
+def test_service_cidr_non_expansion():
+    configure_apiserver('10.152.183.0/24', '10.154.183.0/24', 0)
+
+
+def test_service_cidr_expansion():
+    configure_apiserver('10.152.183.0/24', '10.152.0.0/16', 2)
