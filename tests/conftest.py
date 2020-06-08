fa8a5e017ffd91fcf8886a3e60e4b9aa388808df
Cory Johns <johnsca@gmail.com>
2020-06-08 16:07:17 -0400

    Fix service-cidr expansion race condition
    The logic for handling service-cidr expansion doesn't gracefully handle
    the case where the API server doesn't come up quickly enough. There's
    also some question as to whether the API server might need to be
    restarted after the service is deleted, so check for and handle that.
    
    Fixes https://bugs.launchpad.net/charmed-kubernetes-testing/+bug/1879785

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,4 @@ import charms.unit_test
 charms.unit_test.patch_reactive()
 charms.unit_test.patch_module('charms.coordinator')
 charms.unit_test.patch_module('charms.leadership')
+charms.layer.kubernetes_common.retry.return_value = charms.unit_test.identity
--- tests/test_kubernetes_master.py
+++ tests/test_kubernetes_master.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 import tempfile
 from pathlib import Path
@@ -5,7 +6,7 @@ from unittest import mock
 from reactive import kubernetes_master
 from charms.layer.kubernetes_common import get_version, kubectl
 from charms.layer.kubernetes_master import deprecate_auth_file
-from charms.reactive import endpoint_from_flag, remove_state
+from charms.reactive import endpoint_from_flag, set_flag, is_flag_set
 from charmhelpers.core import hookenv, unitdata
 
 
@@ -57,10 +58,9 @@ def test_send_default_cni():
 
 
 def test_default_cni_changed():
+    set_flag('kubernetes-master.components.started')
     kubernetes_master.default_cni_changed()
-    remove_state.assert_called_once_with(
-        'kubernetes-master.components.started'
-    )
+    assert not is_flag_set('kubernetes-master.components.started')
 
 
 def test_series_upgrade():
@@ -76,28 +76,54 @@ def test_series_upgrade():
 
 @mock.patch('builtins.open', mock.mock_open())
 @mock.patch('os.makedirs', mock.Mock(return_value=0))
-def configure_apiserver(service_cidr_from_db, service_cidr_from_config,
-                        kubectl_call_count):
+def configure_apiserver(service_cidr_from_db, service_cidr_from_config):
+    set_flag('leadership.is_leader')
     db = unitdata.kv()
     db.get.return_value = service_cidr_from_db
     hookenv.config.return_value = service_cidr_from_config
     get_version.return_value = (1, 18)
-    kubectl.return_value = '{"items": []}'.encode('UTF-8')
     kubernetes_master.configure_apiserver()
-    assert kubectl.call_count == kubectl_call_count
+
+
+def update_for_service_cidr_expansion():
+    def _svc(clusterIP):
+        return json.dumps({
+            "items": [
+                {
+                    "metadata": {"name": "kubernetes"},
+                    "spec": {"clusterIP": clusterIP},
+                }
+            ]
+        }).encode('utf8')
+
+    kubectl.side_effect = [
+        _svc('10.152.183.1'),
+        None,
+        _svc('10.152.0.1'),
+        b'{"items":[]}',
+    ]
+    assert kubectl.call_count == 0
+    kubernetes_master.update_for_service_cidr_expansion()
 
 
 def test_service_cidr_greenfield_deploy():
-    configure_apiserver(None, '10.152.183.0/24', 0)
+    configure_apiserver(None, '10.152.183.0/24')
+    assert not is_flag_set('kubernetes-master.had-service-cidr-expanded')
 
 
 def test_service_cidr_no_change():
-    configure_apiserver('10.152.183.0/24', '10.152.183.0/24', 0)
+    configure_apiserver('10.152.183.0/24', '10.152.183.0/24')
+    assert not is_flag_set('kubernetes-master.had-service-cidr-expanded')
 
 
 def test_service_cidr_non_expansion():
-    configure_apiserver('10.152.183.0/24', '10.154.183.0/24', 0)
+    configure_apiserver('10.152.183.0/24', '10.154.183.0/24')
+    assert not is_flag_set('kubernetes-master.had-service-cidr-expanded')
 
 
 def test_service_cidr_expansion():
-    configure_apiserver('10.152.183.0/24', '10.152.0.0/16', 2)
+    configure_apiserver('10.152.183.0/24', '10.152.0.0/16')
+    assert is_flag_set('kubernetes-master.had-service-cidr-expanded')
+    unitdata.kv().get.return_value = '10.152.0.0/16'
+    update_for_service_cidr_expansion()
+    assert kubectl.call_count == 4
