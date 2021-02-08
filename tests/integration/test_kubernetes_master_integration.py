688c9447d8d71e936ad2385759fa0b6139f8232a
Adam Stokes <51892+adam-stokes@users.noreply.github.com>
2021-02-08 12:48:01 -0500

    Support for pytest-operator and integration testing
    Signed-off-by: Adam Stokes <51892+adam-stokes@users.noreply.github.com>

---
+++ tests/integration/test_kubernetes_master_integration.py
@@ -0,0 +1,19 @@
+from pytest_operator import OperatorTest
+
+
+class KubernetesMasterIntegrationTest(OperatorTest):
+    async def test_build_and_deploy(self):
+        bundle = self.render_bundle("tests/data/bundle.yaml",
+                                    master_charm=await self.build_charm("."))
+        await self.model.deploy(bundle)
+        await self.model.wait_for_idle(timeout=4800)
+
+    async def test_status_messages(self):
+        """ Validate that the status messages are correct. """
+        expected_messages = {
+            "kubernetes-master": "Kubernetes master running.",
+            "kubernetes-worker": "Kubernetes worker running.",
+        }
+        for app, message in expected_messages.items():
+            for unit in self.model.applications[app].units:
+                assert unit.workload_status_message == message
