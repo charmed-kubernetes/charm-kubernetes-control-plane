e70aecdc6e628c4b537c6c45a509e5cf5b2066dc
Cory Johns <johnsca@gmail.com>
2021-02-10 16:02:04 -0500

    Show INFO logs and wait for workload status

--- tests/integration/test_kubernetes_master_integration.py
+++ tests/integration/test_kubernetes_master_integration.py
@@ -7,7 +7,11 @@ class KubernetesMasterIntegrationTest(OperatorTest):
             "tests/data/bundle.yaml", master_charm=await self.build_charm(".")
         )
         await self.model.deploy(bundle)
-        await self.model.wait_for_idle(timeout=4800)
+        await self.model.wait_for_idle(timeout=30 * 60)
+        await self.model.block_until(
+            lambda: all(unit.workload_status == "active" for unit in self.model.units),
+            timeout=10 * 60,
+        )
 
     async def test_status_messages(self):
         """ Validate that the status messages are correct. """
