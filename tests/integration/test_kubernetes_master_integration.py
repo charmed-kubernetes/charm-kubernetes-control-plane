857a357d82da2baae41f9f27cc9bea6452961081
Cory Johns <johnsca@gmail.com>
2021-02-10 16:41:42 -0500

    Fix and add logging to post-idle cluster wait

--- tests/integration/test_kubernetes_master_integration.py
+++ tests/integration/test_kubernetes_master_integration.py
@@ -1,4 +1,9 @@
+import logging
+
 from pytest_operator import OperatorTest
+
+
+log = logging.getLogger(__name__)
 
 
 class KubernetesMasterIntegrationTest(OperatorTest):
@@ -8,9 +13,23 @@ class KubernetesMasterIntegrationTest(OperatorTest):
         )
         await self.model.deploy(bundle)
         await self.model.wait_for_idle(timeout=30 * 60)
+
+        def _check_statuses():
+            busy_units = []
+            for unit in self.model.units.values():
+                if unit.workload_status != "active":
+                    busy_units.append(f"{unit.name}: {unit.workload_status}")
+            if busy_units:
+                s = "s" if len(busy_units) > 1 else ""
+                busy_units = ", ".join(busy_units)
+                log.info(f"Waiting for unit{s}: {busy_units}")
+                return False
+
+        log.info("Model is idle, waiting for cluster to be ready")
         await self.model.block_until(
-            lambda: all(unit.workload_status == "active" for unit in self.model.units),
+            _check_statuses,
             timeout=10 * 60,
+            wait_period=60,
         )
 
     async def test_status_messages(self):
