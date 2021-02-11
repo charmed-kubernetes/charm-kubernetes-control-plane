b02c215bab10dbfc8c9d9ede4065d973823110b3
Cory Johns <johnsca@gmail.com>
2021-02-11 10:02:19 -0500

    Update test to use new helper from juju/python-libjuju#473

--- tests/integration/test_kubernetes_master_integration.py
+++ tests/integration/test_kubernetes_master_integration.py
@@ -12,25 +12,7 @@ class KubernetesMasterIntegrationTest(OperatorTest):
             "tests/data/bundle.yaml", master_charm=await self.build_charm(".")
         )
         await self.model.deploy(bundle)
-        await self.model.wait_for_idle(timeout=30 * 60)
-
-        def _check_statuses():
-            busy_units = []
-            for unit in self.model.units.values():
-                if unit.workload_status != "active":
-                    busy_units.append(f"{unit.name}: {unit.workload_status}")
-            if busy_units:
-                s = "s" if len(busy_units) > 1 else ""
-                busy_units = ", ".join(busy_units)
-                log.info(f"Waiting for unit{s}: {busy_units}")
-                return False
-
-        log.info("Model is idle, waiting for cluster to be ready")
-        await self.model.block_until(
-            _check_statuses,
-            timeout=10 * 60,
-            wait_period=60,
-        )
+        await self.model.wait_for_idle(wait_for_active=True, timeout=60 * 60)
 
     async def test_status_messages(self):
         """ Validate that the status messages are correct. """
