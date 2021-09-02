7402b527ea066837c3247a769882f89a2a96e4ba
George Kraft <george.kraft@canonical.com>
2021-09-02 13:19:22 -0500

    Call juju scp with --debug

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -12,7 +12,7 @@ log = logging.getLogger(__name__)
 async def kubernetes(ops_test):
     kubeconfig_path = ops_test.tmp_path / "kubeconfig"
     retcode, stdout, stderr = await ops_test.run(
-        "juju", "scp", "kubernetes-master/leader:config", kubeconfig_path
+        "juju", "scp", "--debug", "kubernetes-master/leader:config", kubeconfig_path
     )
     if retcode != 0:
         log.error(f"retcode: {retcode}")
