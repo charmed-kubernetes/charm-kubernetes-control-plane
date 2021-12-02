e1eb0834090a1225701a9b4f51eb5002e7ff3c18
Adam Dyess <adam.dyess@canonical.com>
2021-12-02 14:42:56 -0600

    Restrict typing_extentions < 4.0 (#188)
    adjust wheelhouse.txt to scope aiohttp to use typing_extentions prior to 4.0

--- tests/validate-wheelhouse.sh
+++ tests/validate-wheelhouse.sh
@@ -4,5 +4,5 @@ build_dir="$(mktemp -d)"
 function cleanup { rm -rf "$build_dir"; }
 trap cleanup EXIT
 
-charm build . --build-dir "$build_dir"
+charm build . --build-dir "$build_dir" --debug
 pip install -f "$build_dir/kubernetes-master/wheelhouse" --no-index --no-cache-dir "$build_dir"/kubernetes-master/wheelhouse/*
