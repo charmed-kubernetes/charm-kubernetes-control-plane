2d2a41e117f7f21786f686da53de9c9495a6f35f
Stone Preston <stone.preston@canonical.com>
2022-08-22 15:15:00 -0500

    Fix linting

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -50,15 +50,15 @@ def k8s_core_bundle(ops_test):
 @pytest.fixture(scope="module")
 @pytest.mark.asyncio
 async def k8s_core_yaml(ops_test, k8s_core_bundle):
-    """Download and render the kubernetes-core bundle, return it's full yaml`
-    bundle_path,  = await ops_test.async_render_bundles(k8s_core_bundle)
+    """Download and render the kubernetes-core bundle, return it's full yaml"""
+    (bundle_path,) = await ops_test.async_render_bundles(k8s_core_bundle)
     return yaml.safe_load(bundle_path.read_text())
 
 
 @pytest.fixture(scope="module")
 def series(k8s_core_yaml, request):
     series = request.config.getoption("--series")
-     return series if series else k8s_core_yaml["series"]
+    return series if series else k8s_core_yaml["series"]
 
 
 @pytest.fixture(scope="module")
