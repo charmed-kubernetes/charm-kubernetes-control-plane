2fd98e8f5f42d0462c80f28fa3d056786b40c875
Adam Dyess <adam.dyess@canonical.com>
2022-09-23 14:50:04 -0500

    CSIMigrationAzureDisk cannot be set False in 1.25+ (#251)
    * CSIMigrationAzureDisk cannot be set False in 1.25+
    
    * catch every mention of CSIMigrationAzureDisk

--- reactive/kubernetes_control_plane.py
+++ reactive/kubernetes_control_plane.py
@@ -2463,7 +2463,8 @@ def configure_apiserver():
     elif is_state("endpoint.azure.ready"):
         api_opts["cloud-provider"] = "azure"
         api_opts["cloud-config"] = str(api_cloud_config_path)
-        feature_gates.append("CSIMigrationAzureDisk=false")
+        if get_version("kube-apiserver") < (1, 25, 0):
+            feature_gates.append("CSIMigrationAzureDisk=false")
 
     api_opts["feature-gates"] = ",".join(feature_gates)
 
@@ -2643,7 +2644,8 @@ def configure_controller_manager():
     elif is_state("endpoint.azure.ready"):
         controller_opts["cloud-provider"] = "azure"
         controller_opts["cloud-config"] = str(cm_cloud_config_path)
-        feature_gates.append("CSIMigrationAzureDisk=false")
+        if get_version("kube-apiserver") < (1, 25, 0):
+            feature_gates.append("CSIMigrationAzureDisk=false")
 
     controller_opts["feature-gates"] = ",".join(feature_gates)
 
@@ -2675,7 +2677,8 @@ def configure_scheduler():
         if get_version("kube-apiserver") < (1, 25, 0):
             feature_gates.append("CSIMigrationGCE=false")
     elif is_state("endpoint.azure.ready"):
-        feature_gates.append("CSIMigrationAzureDisk=false")
+        if get_version("kube-apiserver") < (1, 25, 0):
+            feature_gates.append("CSIMigrationAzureDisk=false")
     elif is_state("endpoint.vsphere.ready") and get_version("kube-apiserver") >= (
         1,
         12,
