eb6af468df000fae990d11148844d0b9120d5732
George Kraft <george.kraft@canonical.com>
2023-07-07 11:55:26 -0500

    charmcraft init

---
+++ tests/unit/test_charm.py
@@ -0,0 +1,23 @@
+# Copyright 2023 George Kraft
+# See LICENSE file for licensing details.
+#
+# Learn more about testing at: https://juju.is/docs/sdk/testing
+
+import unittest
+
+import ops
+import ops.testing
+from charm import KubernetesControlPlaneCharm
+
+
+class TestCharm(unittest.TestCase):
+    def setUp(self):
+        self.harness = ops.testing.Harness(KubernetesControlPlaneCharm)
+        self.addCleanup(self.harness.cleanup)
+
+    def test_start(self):
+        # Simulate the charm starting
+        self.harness.begin_with_initial_hooks()
+
+        # Ensure we set an ActiveStatus with no message
+        self.assertEqual(self.harness.model.unit.status, ops.ActiveStatus())
