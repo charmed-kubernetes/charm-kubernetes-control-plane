e98f128abb539226a1c1a7e73da84b7c9e667e1f
George Kraft <george.kraft@canonical.com>
2023-07-17 11:28:23 -0500

    make linters happy

--- src/charm.py
+++ src/charm.py
@@ -2,26 +2,27 @@
 # Copyright 2023 Canonical
 # See LICENSE file for licensing details.
 
-import logging
-import ops
+"""Charm."""
 
+import logging
+
+import ops
 from charms import kubernetes_snaps
 from charms.reconciler import Reconciler
-
 
 log = logging.getLogger(__name__)
 
 
 class KubernetesControlPlaneCharm(ops.CharmBase):
+    """Charm."""
+
     def __init__(self, *args):
         super().__init__(*args)
         self.reconciler = Reconciler(self, self.reconcile)
 
     def reconcile(self, event):
-        kubernetes_snaps.install(
-            channel=self.model.config['channel'],
-            control_plane=True
-        )
+        """Reconcile state change events."""
+        kubernetes_snaps.install(channel=self.model.config["channel"], control_plane=True)
 
 
 if __name__ == "__main__":  # pragma: nocover
