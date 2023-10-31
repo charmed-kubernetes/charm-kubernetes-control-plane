fcaacbb6444da79ab8d7dab1db59858d0778bd8a
Mateo Florido <mateo.florido@canonical.com>
2023-10-31 12:54:17 -0500

    Add `cni-plugins` resource

---
+++ tests/integration/test_k8s_control_plane_charm.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical
+# See LICENSE file for licensing details.
+
+import logging
+from pathlib import Path
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest):
+    """Build kubernetes-control-plane and deploy with kubernetes-core bundle."""
+    charm = next(Path().glob("kubernetes-control-plane*.charm"), None)
+    if not charm:
+        log.info("Building charm")
+        charm = await ops_test.build_charm(".")
+
+    resource_path = ops_test.tmp_path / "charm-resources"
+    resource_path.mkdir()
+    resource_build_script = Path("./build-cni-resources.sh").resolve()
+    log.info("Building charm resources")
+    rc, stdout, stderr = await ops_test.run(str(resource_build_script), cwd=resource_path)
+    if rc != 0:
+        log.error(f"rc: {rc}\nstdout: {stdout}\nstderr: {stderr}")
+        pytest.fail("Failed to build charm resources")
+
+    log.info("Building bundle")
+    bundle, *overlays = await ops_test.async_render_bundles(
+        ops_test.Bundle("kubernetes-core", channel="edge"),
+        Path("tests/data/charm.yaml"),
+        arch="amd64",
+        charm=charm.resolve(),
+        resource_path=resource_path,
+    )
+
+    log.info("Deploying bundle")
+    cmd = ["juju", "deploy", "-m", ops_test.model_full_name, bundle]
+    for overlay in overlays:
+        cmd += ["--overlay", overlay]
+    rc, stdout, stderr = await ops_test.run(*cmd)
+    assert rc == 0, f"Bundle deploy failed: {(stderr or stdout).strip()}"
+
+    await ops_test.model.wait_for_idle(status="active", timeout=60 * 60)
