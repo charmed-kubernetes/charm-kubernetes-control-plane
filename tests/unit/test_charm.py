f5321302fcbb1d5ae340e765026d177bc8dc1788
Adam Dyess <adam.dyess@canonical.com>
2023-11-02 16:26:38 -0500

    Label control-plane nodes

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -4,7 +4,7 @@
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
 import json
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 import ops
 import ops.testing
@@ -42,7 +42,13 @@ def harness():
 @patch("charms.kubernetes_snaps.write_etcd_client_credentials")
 @patch("charms.kubernetes_snaps.write_service_account_key")
 @patch("charm.KubernetesControlPlaneCharm.install_cni_binaries")
+@patch("charms.node_base.LabelMaker.active_labels")
+@patch("charms.node_base.LabelMaker.set_label")
+@patch("charms.node_base.LabelMaker.remove_label")
 def test_active(
+    remove_label,
+    set_label,
+    active_labels,
     install_cni_binaries,
     write_service_account_key,
     write_etcd_client_credentials,
@@ -64,6 +70,7 @@ def test_active(
     auth_webhook_configure,
     harness,
 ):
+    active_labels.return_value = {}
     get_dns_address.return_value = "10.152.183.10"
     get_public_address.return_value = "10.0.0.10"
     hash_file.return_value = "test-hash"
@@ -206,3 +213,10 @@ def test_active(
         ca="test-etcd-ca", cert="test-etcd-client-cert", key="test-etcd-client-key"
     )
     write_service_account_key.assert_called_once_with("test-service-account-key")
+    
+    set_label.assert_has_calls([
+        call("node-role.kubernetes.io/control-plane", ""),
+        call("juju-application", "kubernetes-control-plane"),
+        call("juju-charm", "kubernetes-control-plane"),
+    ])
+    remove_label.assert_has_calls([call("juju.io/cloud")])
