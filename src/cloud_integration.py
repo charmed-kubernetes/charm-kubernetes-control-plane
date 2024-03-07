9f96219e0ba79e3305fe09df3fcd50a944e0fce1
Adam Dyess <adam.dyess@canonical.com>
2024-03-07 12:33:21 -0600

    black formatting

--- src/cloud_integration.py
+++ src/cloud_integration.py
@@ -9,9 +9,8 @@ from typing import Union
 import charms.contextual_status as status
 import ops
 from ops.interface_aws.requires import AWSIntegrationRequires
-from ops.interface_gcp.requires import GCPIntegrationRequires
 from ops.interface_azure.requires import AzureIntegrationRequires
-
+from ops.interface_gcp.requires import GCPIntegrationRequires
 
 log = logging.getLogger(__name__)
 
@@ -40,11 +39,7 @@ class CloudIntegration:
     def cloud(self) -> Union[None, AWSIntegrationRequires, GCPIntegrationRequires]:
         """Determine if we're integrated with a known cloud."""
         cloud_name = self.charm.get_cloud_name()
-        cloud_support = {
-            "aws": self.aws,
-            "gce": self.gcp,
-            "azure": self.azure
-        }
+        cloud_support = {"aws": self.aws, "gce": self.gcp, "azure": self.azure}
         if not (cloud := cloud_support.get(cloud_name)):
             log.error("Skipping Cloud integration: unsupported cloud %s", cloud_name)
             return
